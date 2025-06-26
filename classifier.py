9cd2d7ab933e880b733d469d7d4d25d400be4640
shamrocksprings10 <ayush.sharma.se@gmail.com>
2025-06-25 20:03:29 -0500

    finished all probability methods for bayes classifier.py

--- classifier.py
+++ classifier.py
@@ -1,7 +1,8 @@
 import numpy as np
+from scipy.special import factorial
 from collections import Counter
 
-def word_frequencies(self, features: list[str]) -> dict[str, int]:
+def word_frequencies(features: list[str]) -> dict[str, int]:
     return dict(Counter(features))
 
 def ensure_frequencies(function):
@@ -10,7 +11,7 @@ def ensure_frequencies(function):
     always be a dict[str, int] even if a list[str] is passed. This
     decorator is only meant for classes inside of classifier.py.
     """
-    def wrapped(self, list_arg, *args, **kwargs):
+    def wrapped(self, list_arg: dict[str, int] | list[str], *args, **kwargs):
         if type(list_arg) is list:
             list_arg = word_frequencies(list_arg)
         function(self, list_arg, *args, **kwargs)
@@ -30,15 +31,26 @@ class _BayesClassifier:
         return self.class_probs[self.class_labels.index(class_)]
 
     @ensure_frequencies
-    def prob_class_given_features(self, features: dict[str, int] | list[str], class_: str) -> float:
+    def prob_class_given_features(self, features: dict[str, int], class_: str) -> float:
         # bayes theorem, remove p(x) from denominator since we only care about comparison
-        prob_features_given_class = self.prob_features_given_class(features)
-        return self.prob_class(class_) * prob_features_given_class
+        return self.prob_class(class_) * self.prob_features_given_class(features, class_)
 
     @ensure_frequencies
-    def prob_features_given_class(self, features: dict[str, int] | list[str]):
+    def prob_features_given_class(self, features: dict[str, int], class_: str) -> float:
         # remove n! from multinomial PMF since we're just comparing
-        return
+        words = features.keys()
+        counts = np.array(features.values())
+
+        factor = 1 / np.prod(factorial(counts))
+        prob = np.prod(np.array([self.prob_word_given_class(words[i], class_) ** c_i for i, c_i in enumerate(counts)]))
+        return factor * prob
+
+    @ensure_frequencies
+    def prob_classes_given_features(self, features: dict[str, int]):
+        return np.array([self.prob_class_given_features(features, class_) for class_ in self.num_classes])
+
+    def train(self):
+        pass
 
 class BayesClassifier(_BayesClassifier):
     def __init__(self, class_labels: list[str], num_classes: int = 2):
