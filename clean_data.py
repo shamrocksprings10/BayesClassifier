a96f1e683937c4ac45470ba689043cd33bd48302
shamrocksprings10 <ayush.sharma.se@gmail.com>
2025-06-26 15:39:36 -0500

    made it work for bbc_documents csv instead

--- clean_data.py
+++ clean_data.py
@@ -1,26 +1,43 @@
+import csv
 import string, re
 import pandas as pd
 
 remove = string.digits + string.punctuation
 
-def get_spam_collection_df():
+def get_document_collection_df():
     rows = []
 
-    with open("SMSSpamCollection.txt", "r") as input_file:
-        for row in input_file:
-            class_, text = row.split(maxsplit=1)
+    with open("bbc_data.csv", "r") as input_file:
+        input_reader = csv.reader(input_file, delimiter=",", quotechar='"')
+        for row in input_reader:
+            text, label = row
 
             cleaned_text = re.sub(rf"[{remove}]+", "", text).lower()
             words = list(filter(lambda s: len(s) > 1, re.findall(r"[a-z]+", cleaned_text)))
 
-            rows.append((class_, words))
+            rows.append((label, words))
 
     return pd.DataFrame(rows, columns=["class", "words"])
 
+def split_train_test(percent_train:float=0.8) -> tuple[pd.DataFrame, pd.DataFrame, list[str]]:
+    df = get_document_collection_df()
+    df = df.sample(frac=1)  # shuffled order
+
+    train_size = int(0.8 * len(df))
+    train_df = df[:train_size]
+    test_df = df[train_size:]
+
+    class_labels = df["class"].unique().tolist()
+
+    return train_df, test_df, class_labels
+
 if __name__ == '__main__':
-    df = get_spam_collection_df()
+    df = get_document_collection_df()
     print(df.head(5))
 
     # Frequency of both ham and spam messages
     print(df["class"].value_counts())
+
+    # test split train/test
+    print(split_train_test())
 
