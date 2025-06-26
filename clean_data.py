1b4b2d9d527b09412cec57c368a366ea14e7e533
shamrocksprings10 <ayush.sharma.se@gmail.com>
2025-06-25 21:27:32 -0500

    data processing and transmission fixed, no .csv anymore!

--- clean_data.py
+++ clean_data.py
@@ -2,21 +2,25 @@ import string, re
 import pandas as pd
 
 remove = string.digits + string.punctuation
-rows = []
 
-with open("SMSSpamCollection.txt", "r") as input_file:
-    for row in input_file:
-        class_, text = row.split(maxsplit=1)
+def get_spam_collection_df():
+    rows = []
 
-        cleaned_text = re.sub(rf"[{remove}]+", "", text).lower()
-        words = list(filter(lambda s: len(s) > 1, re.findall(r"[a-z]+", cleaned_text)))
+    with open("SMSSpamCollection.txt", "r") as input_file:
+        for row in input_file:
+            class_, text = row.split(maxsplit=1)
 
-        rows.append((class_, words))
+            cleaned_text = re.sub(rf"[{remove}]+", "", text).lower()
+            words = list(filter(lambda s: len(s) > 1, re.findall(r"[a-z]+", cleaned_text)))
 
-df = pd.DataFrame(rows, columns=["class", "words"])
-print(df.head(5))
+            rows.append((class_, words))
 
-# Frequency of both ham and spam messages
-print(df["class"].value_counts())
+    return pd.DataFrame(rows, columns=["class", "words"])
 
-df.to_csv("SMSSpamCollection.csv", index=False)
+if __name__ == '__main__':
+    df = get_spam_collection_df()
+    print(df.head(5))
+
+    # Frequency of both ham and spam messages
+    print(df["class"].value_counts())
+
---
+++ test.py
@@ -0,0 +1,13 @@
+from classifier import BayesClassifier
+from clean_data import get_spam_collection_df
+import pandas as pd
+
+df = get_spam_collection_df()
+df = df.sample(frac=1) # shuffled order
+
+train_size = int(0.8 * len(df))
+train_df = df[:train_size]
+test_df = df[train_size:]
+
+model = BayesClassifier(["ham", "spam"])
+model.train(train_df)
